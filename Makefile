0d4899f87464eb020ef50fe8903324c3dd7bd4ac
zhedlund <zhedlund@student.42berlin.de>
2023-12-10 16:13:45 +0100

    added functions

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: zhedlund <zhedlund@student.42berlin.d      +#+  +:+       +#+         #
+#    By: zhedlund <zhedlund@student.42berlin.de>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/11 11:43:57 by zhedlund          #+#    #+#              #
-#    Updated: 2023/05/26 12:46:28 by zhedlund         ###   ########.fr        #
+#    Updated: 2023/12/10 16:12:54 by zhedlund         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,9 +15,10 @@ NAME = libft.a
 SRC =	ft_atoi.c ft_bzero.c ft_calloc.c ft_isalnum.c ft_isalpha.c \
 	ft_isascii.c ft_itoa.c ft_isdigit.c ft_isprint.c ft_memchr.c \
 	ft_memcmp.c ft_memcpy.c ft_memmove.c ft_memset.c ft_split.c \
-	ft_strchr.c ft_strdup.c ft_striteri.c ft_strjoin.c ft_strlcat.c \
-	ft_strlcpy.c ft_strlen.c ft_strmapi.c ft_strncmp.c ft_strnstr.c \
-	ft_strrchr.c ft_strtrim.c ft_substr.c ft_tolower.c ft_toupper.c \
+	ft_strcat.c ft_strcmp.c ft_strchr.c ft_strcpy.c ft_strdup.c \
+	ft_striteri.c ft_strjoin.c ft_strlcat.c ft_strlcpy.c ft_strlen.c \
+	ft_strmapi.c ft_strncmp.c ft_strnstr.c ft_strpbrk.c ft_strrchr.c \
+	ft_strtok.c ft_tolower.c ft_toupper.c ft_strtrim.c ft_substr.c \
 	ft_putchar_fd.c ft_putendl_fd.c ft_putnbr_fd.c ft_putstr_fd.c
 
 BONUS_SRC = ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c \
--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zhedlund <zhedlund@student.42berlin.d      +#+  +:+       +#+        */
+/*   By: zhedlund <zhedlund@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 13:49:19 by zhedlund          #+#    #+#             */
-/*   Updated: 2023/05/26 12:02:58 by zhedlund         ###   ########.fr       */
+/*   Updated: 2023/12/10 16:05:18 by zhedlund         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,10 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
+char	*ft_strcat(char *dest, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
+char	*ft_strcpy(char *dest, const char *src);
 char	*ft_strdup(const char *s);
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
@@ -51,7 +54,9 @@ char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char	*ft_strpbrk(const char *s1, const char *s2);
 char	*ft_strrchr(const char *s, int c);
+char	*ft_strtok(char *str, const char *delim);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_toupper(int c);
